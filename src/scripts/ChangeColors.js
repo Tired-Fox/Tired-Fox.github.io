070b7c071800d1777dce6167fc939565fa374367
Zachary Boehm <zboehm104@gmail.com>
2022-05-20 14:33:49 -0500

    Made starting site

---
+++ src/scripts/ChangeColors.js
@@ -0,0 +1,11 @@
+function toLight() {
+    let body = document.body;
+
+    body.className = 'blight-one tlight-five';
+}
+
+function toDark() {
+    let body = document.body;
+
+    body.className = 'bdark-one tdark-five';
+}
